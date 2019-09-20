0d4c638d093231bcbca7a00a71fa9c51111df212
Javy de Koning <javy@amazon.com>
2019-09-20 09:14:39 -0700

    Quick bugfix for "Authorisation reset". Todo: Better error handling

--- sam-app/bunq2ynab/app.py
+++ sam-app/bunq2ynab/app.py
@@ -8,18 +8,18 @@ from bunq2ynab import sync
 
 LOGGER = configure_logger(__name__)
 
-#Set configuration file path
-config_location  = 'ssm:' + os.getenv('SSM_PARAM', '/lambda/bunq2ynab')
+# Set configuration file path
+config_location = 'ssm:' + os.getenv('SSM_PARAM', '/lambda/bunq2ynab')
 
-#Initialize Config, Bunq and YNAB
+# Initialize Config, Bunq and YNAB
 config = configuration(config_location)
 b = bunqapi(config_location)
 y = ynabapi(config_location)
 
 
-#Start program
+# Start program
 def lambda_handler(event, context):
-    LOGGER.info("Starting Lambda")
+    LOGGER.info("Starting Lambda!!")
 
     if int(os.getenv('LIST_MODE', False)):
         LOGGER.info('Running in LIST mode, no accounts will be synced.')
@@ -28,9 +28,19 @@ def lambda_handler(event, context):
     else:
         for i in config.value['bunq2ynab']:
             for key, value in i.items():
-                assert value, '{0} cannot be an empty string. Setup your sync pairs in your config'.format(key)
+                assert value, '{0} cannot be an empty string. Setup your sync pairs in your config'.format(
+                    key)
 
-        sync(config, b, y)
+        try:
+            LOGGER.info('Running in SYNC mode.')
+            sync(config, b, y)
+        except:
+            LOGGER.info('Detected authorization error, resetting config')
+            config.value['bunq']['install_token'] = ''
+            config.value['bunq']['priv_key'] = ''
+            config.value['bunq']['server_pub_key'] = ''
+            config.value['bunq']['session_token'] = ''
+            config.save(config.value)
 
     return {
         "statusCode": 200,
